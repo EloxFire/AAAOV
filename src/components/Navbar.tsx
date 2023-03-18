45401ad107a07cb079abb67b381ba11d01da54da
Enzo Avagliano <enzo.avagliano@ynov.com>
2023-03-18 17:04:15 +0100

    Created navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import React from 'react'
+import navbarStyles from '../styles/components/Navbar.module.scss'
+
+interface NavbarProps {
+  logo?: string
+  links: { label: string, href: string }[]
+}
+
+export default function Navbar({ logo, links }: NavbarProps) {
+  return (
+    <nav id="navbar" className={navbarStyles.navbar}>
+      <div className="navbar-logo-container">
+        {
+          logo ?
+            <Image alt='Logo des AAAOV' src={logo} className={navbarStyles.logo} />
+            :
+            <h1>AAAOV</h1>
+        }
+      </div>
+      <div className="navbar-links-container">
+        <ul className={navbarStyles.navbarLinks}>
+          {
+            links.map((link, index) => {
+              return (
+                <a key={'navbar-link-${index}'} href={link.href} className={navbarStyles.navbarLink}>{link.label}</a>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </nav>
+  )
+}
